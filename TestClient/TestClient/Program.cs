3ba8b43c6b369864b35249cff1426902f18b94b9
David Hartley <dave@vixtro.com.au>
2017-03-17 10:47:25 +1100

    Initial commit

---
+++ TestClient/TestClient/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = Connection.Host;
+            var community = Connection.Community;
+        }
+    }
+}
